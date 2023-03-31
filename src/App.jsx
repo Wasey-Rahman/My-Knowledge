f80e809002b9e48c14083ffdd9ca3cabde0279ff
Wasey Rahman <wasey123435@gmail.com>
2023-04-01 01:08:46 +0600

    Learn-Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Learn from './components/Learn/Learn';
 
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
     <Header></Header>
+    <Learn></Learn>
       </div>
   )
 }
---
+++ src/components/Learn/Learn.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Learn.css'
+const Learn = () => {
+    return (
+        <div className='Learn-container'>
+            <div className="item-container">
+
+            </div>
+            <div className="time">
+             <h4 className='Title'>Spent time on read :   min</h4>
+             <div className=" bookmark-container">
+              <h4>Bookmarked Blogs : </h4>
+                    
+
+             </div>
+            </div>
+        </div>
+    );
+};
+
+export default Learn;
