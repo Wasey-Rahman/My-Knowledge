3003107a9c61ae647c97708d4adafcdd0670c6fd
Wasey Rahman <wasey123435@gmail.com>
2023-04-01 00:27:07 +0600

    Heading-Bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,12 @@ const Header = () => {
     return (
         <div className='Header'>
             <h2 className='Heading'>Knowledge Cafe</h2>
-            
+            <div className='bar'>
+             <a href="/Home" className='Link'>Home</a>
+             <a href="/Inventory"className='Link'>Inventory</a>
+             <a href="/Login"className='Link'>Login</a>
+
+            </div>
             
         </div>
     );
