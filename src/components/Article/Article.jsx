c928ebc190a340d84e74671425fceba0c3f29d1e
Wasey Rahman <wasey123435@gmail.com>
2023-04-01 19:27:28 +0600

    Icon Adding

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookBookmark } from '@fortawesome/free-solid-svg-icons'
+
 import './Article.css'
 const Article = (props) => {
     const {authorName,blogTitle,blogCoverImage,authorImage,readTime,publishDate} = props.item;
@@ -9,12 +12,12 @@ const Article = (props) => {
     return (
         <div className='item'>
            <img src={blogCoverImage} alt="" />
-           <h6 className='Read'>{readTime}</h6>
+           <h6 className='Read'>{readTime}</h6><button onClick={() =>handleAddToSpendTime()} className='Bookmark'><FontAwesomeIcon icon={faBookBookmark} /></button>
            <img src={authorImage} alt="" className='Author-img'/>
            <h3 className='Author-name'>{authorName}</h3>
           <h6 className='Author-name'>{publishDate}</h6>
            <h2>{blogTitle}</h2>
-           <button onClick={handleAddToSpendTime} className='Mark'>Mark as read</button>
+           <button onClick={() =>handleAddToSpendTime()} className='Mark'>Mark as read</button>
            
           
           
