d0f05456035f8e85c7c2bca078e27b6541cacf7a
Wasey Rahman <wasey123435@gmail.com>
2023-04-01 16:50:40 +0600

    Updating images and author names

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,9 +3,12 @@ import './Article.css'
 const Article = (props) => {
     const {authorName,blogTitle,blogCoverImage,authorImage,readTime,publishDate} = props.item;
     return (
-        <div className='article'>
-           <img src={blogCoverImage} alt="" /> 
-          {/* <img src={authorImage} alt="" /> */}
+        <div className='item'>
+           <img src={blogCoverImage} alt="" />
+           <img src={authorImage} alt="" className='Author-img'/>
+           <h3 className='Author-name'>{authorName}</h3>
+           
+          
           
         </div>
     );
